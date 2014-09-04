16fae3a04bb251e719eb720fbbe58c676b415a41
stephanie pi <stephmpi@gmail.com>
2014-09-03 22:37:15 -0700

    mynameis app added to repo

---
+++ mynameis.rb
@@ -0,0 +1,10 @@
+puts 'Hello, new friend! What\'s your name?'
+name = gets.chomp
+puts 'Did you say ' + name +  '? What a lovely name!'
+puts 'Do you have a middle name?'
+middle_name = gets.chomp
+puts 'I have a cousin named ' + middle_name + '!'
+puts 'What\'s your last name?'
+last_name = gets.chomp
+puts 'I had a teacher with the last name ' + last_name + '.'
+puts 'Well, it\'s been really nice chatting ' + name + ' ' + middle_name + ' '+ last_name + '.'
