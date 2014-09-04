e1b4186e7d4b94f2b43882c46d05999015ac9351
stephanie pi <stephmpi@gmail.com>
2014-09-03 22:47:29 -0700

    added best number app

---
+++ bestnumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+best_num = gets.chomp
+bestest_num = best_num.to_i + 1
+puts 'Oh, you probably don\'t know about ' + bestest_num.to_s + '. It\'s like way better.'
